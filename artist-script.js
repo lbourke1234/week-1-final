4c319c165aeacb0791ab1d8484c4abef473fbbe2
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-03-31 11:52:18 +0100

    Update - Rasmus

---
+++ artist-script.js
@@ -0,0 +1,16 @@
+
+
+let addSongToLikedAlbum = (event) => {
+    let heart = document.querySelector("body > div > div.all-except-sidebar > div.play-stop-and-table-container > div.container-fluid.table-container > div > div.col-9.heading-and-table > table > thead > tr > th:nth-child(5) > i");
+    console.log(event.target)
+/*      alert(event.target) */
+
+}
+
+
+
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
