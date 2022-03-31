3c9bf6e5e92fd23b57823a7afb5963949daa1b76
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-03-31 13:15:45 +0100

    Update

--- artist-script.js
+++ artist-script.js
@@ -1,16 +1,14 @@
-
+/* 
 
 let addSongToLikedAlbum = (event) => {
     let heart = document.querySelector("body > div > div.all-except-sidebar > div.play-stop-and-table-container > div.container-fluid.table-container > div > div.col-9.heading-and-table > table > thead > tr > th:nth-child(5) > i");
     console.log(event.target)
 /*      alert(event.target) */
 
-}
 
 
 
-
-function myFunction() {
+function addSongToLikedAlbum() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
   }
