50b69da8540cb69a904f319090b619436d8e540b
lbourke1234 <lbourke1234@gmail.com>
2022-04-01 13:02:32 +0100

    final push from leon

--- homepagefinal.js
+++ homepagefinal.js
@@ -1,3 +1,45 @@
 window.onload = function() {
-    document.querySelectorAll('.card')
+    addGreenPlayImgEventListener()
+    addListenerToLoveHeart()
+    addListenerToFirstButton()
+    }
+
+
+
+function addGreenPlayImgEventListener() {
+    let greenPlayNode = document.querySelectorAll('.green-play-img')
+    for (let play of greenPlayNode) {
+        play.addEventListener('click', addNewArtistToNowPlaying)
+    }
+}
+
+function addNewArtistToNowPlaying(e) {
+    let artistName = e.target.closest('.card').childNodes[5].childNodes[1].innerText
+    let songName = e.target.closest('.card').childNodes[5].childNodes[3].innerText
+    let currentArtistNode = document.querySelector('.p-artist-name')
+    let currentSongNode = document.querySelector('.p-song-name')
+    currentArtistNode.innerText = artistName
+    currentSongNode.innerText = songName
+}
+
+function addListenerToLoveHeart() {
+    document.querySelector('.bi-heart').addEventListener('click', showOrHideHeart)
+    document.querySelector('.js-heart').addEventListener('click', showOrHideHeart)
+}
+function showOrHideHeart() {
+    document.querySelector('footer i:first-of-type').classList.toggle('visible')
+}
+
+function addListenerToFirstButton () {
+    let seeAllBtnNode = document.querySelectorAll('.see-all-btn')
+    seeAllBtnNode[0].addEventListener('click', addNewRow)
+    console.log(seeAllBtnNode)
+}
+
+function addNewRow() {
+    let newRowNode = document.querySelector('.hidden-row')
+    newRowNode.classList.toggle('second-section-card-hidden')
+    newRowNode.classList.toggle('new-row-animation')
+
+    console.log('done')
 }
